3989c5f42eeb702e73798f03a012a9b8e169aa8a
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-12-02 16:01:19 -0300

    Requisito 7 | Feito

--- src/challenges.js
+++ src/challenges.js
@@ -57,8 +57,16 @@ function highestCount(arrNumeros) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaCat1 = Math.abs(cat1 - mouse);
+  let distanciaCat2 = Math.abs(cat2 - mouse);
+
+  if (distanciaCat1 < distanciaCat2)
+    return "cat1";
+  else if (distanciaCat2 < distanciaCat1)
+    return "cat2";
+  else
+    return "os gatos trombam e o rato foge";
 }
 
 // Desafio 8
