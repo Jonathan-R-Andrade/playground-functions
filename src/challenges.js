46ec9d6fa161982c5689a87dd41a1683824dbb47
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-12-02 15:37:10 -0300

    Requisito 5 | Feito

--- src/challenges.js
+++ src/challenges.js
@@ -25,8 +25,8 @@ function concatName(arrStrings) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
