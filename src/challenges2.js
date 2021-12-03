58ed2bba9b0fdabfe8d1cdfd1df18bfcd8024581
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-12-03 00:23:49 -0300

    Requisito 11 | Feito

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,8 +16,59 @@ function techList(listTech, namePerson) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function contarRepeticoes(arrNumeros) {
+  let numeroRepeticoes = {};
+  for (let i = 0; i < arrNumeros.length; i += 1) {
+    let numero = arrNumeros[i];
+    if (numeroRepeticoes[numero] === undefined) {
+      numeroRepeticoes[numero] = 1;
+    } else {
+      numeroRepeticoes[numero] += 1;
+    }
+  }
+  return numeroRepeticoes;
+}
+
+function verficarRepeticaoMaxima(repeticoes) {
+  let maxRepeticao = 0;
+  for (let key in repeticoes) {
+    if (repeticoes[key] > maxRepeticao) {
+      maxRepeticao = repeticoes[key];
+    }
+  }
+  return maxRepeticao;
+}
+
+function numeroInvalido(numbers) {
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (Number(numbers[i]) < 0 || Number(numbers[i]) > 9) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function formarNumero(arrPhoneNumbers) {
+  let phoneNumber = '('.concat(arrPhoneNumbers[0], arrPhoneNumbers[1], ') ');
+  for (let i = 2; i < 7; i += 1) {
+    phoneNumber += arrPhoneNumbers[i];
+  }
+  phoneNumber += '-';
+  for (let i = 7; i < 11; i += 1) {
+    phoneNumber += arrPhoneNumbers[i];
+  }
+  return phoneNumber;
+}
+
+function generatePhoneNumber(arrPhoneNumbers) {
+  if (arrPhoneNumbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let repeticaoMaxima = verficarRepeticaoMaxima(contarRepeticoes(arrPhoneNumbers));
+  if (repeticaoMaxima > 2 || numeroInvalido(arrPhoneNumbers)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return formarNumero(arrPhoneNumbers);
 }
 
 // Desafio 12
